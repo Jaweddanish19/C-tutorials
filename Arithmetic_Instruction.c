08cd2c69b5aa28016e39050a9f94726e7f9ab767
MD DANISH JAWED <95688212+Jaweddanish19@users.noreply.github.com>
2023-01-01 00:30:36 +0530

    Add files via upload

---
+++ Arithmetic_Instruction.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int a=2,b=3;
+    int sum=a+b;
+    int multiply=a*b;
+    int power=pow(a,b);
+    printf("%d",power);
+    return 0;
+}
---
+++ Type_declaration_instruction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int a=1;
+    int b=a+2;
+    int c=b-a;
+    int d=c*b;
+    int e=1,f;
+
+    int oldage=22;
+    int years=2;
+    // we have to declare a variable at first then after we can use the variable such as "years " here
+    int newage=oldage+years;
+
+    int x,y,z;
+    x=y=z=1;
+return 0;
+}

77af2d09a98b60f9f32bcf8f9f83c1ced07a81b4
MD DANISH JAWED <95688212+Jaweddanish19@users.noreply.github.com>
2023-01-01 23:57:05 +0530

    Add files via upload

---
+++ Modulator_operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("%d\n",8%3);
+    printf("%d",-6%4);
+    return 0;
+
+}
+
+// Modulo operator does not work for float values.Modulo operator works only for integer values.
---
+++ Type_conversion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    printf("%d\n",2*2);
+    printf("%f\n",2*2.0);
+    printf("%f\n",2.0*2.0);
+    
+    return 0;
+
+}
+
+/*
+TYPE CONVERSION-
+1.int operations on int -> int
+e.g. 2*2 -> 4
+2.int operations on float -> float
+e.g. 2*2.0 -> 4.0
+3.float opeartions on float -> float
+e.g. 2.0*2.0 -> 4.0
+
+*/
+

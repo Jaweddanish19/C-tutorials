9fbb09570165a7f5652a028751880e49c3b64b98
MD DANISH JAWED <95688212+Jaweddanish19@users.noreply.github.com>
2023-01-04 00:07:35 +0530

    Update Relational_operators.c

--- Relational_operators.c
+++ Relational_operators.c
@@ -11,6 +11,10 @@ int main (){
     printf("%d\n",4==4);
     printf("%d\n",4>3);
     printf("%d\n",4<3);
+    
+    //if we assign an equal to operator with less than or greater than operator then compiler will check both the operations equal to as well as greater or less than operators
+    printf("%d\n",4<=3);
+    printf("%d\n",4>=3);
 
     return 0;
 

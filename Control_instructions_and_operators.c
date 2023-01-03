0b6668957ee2915b66a586e72214605ca74fce5e
MD DANISH JAWED <95688212+Jaweddanish19@users.noreply.github.com>
2023-01-04 00:00:13 +0530

    Add files via upload

---
+++ Control_instructions_and_operators.c
@@ -0,0 +1,17 @@
+/*
+Contorl instructions -It is used to determine flow of program
+A.SEQUENCE CONTORL -> Instructions are given in sequence one by one
+B.DECISION CONTROL -> If else etc.
+C.LOOP CONTROL -> For loop,while loop etc.
+D.CASE CONTROL -> Instructions are given according to case
+*/
+
+/*OPERATORS
+
+A.ARITHMETIC OPERATORS -> +,-,/,*,%
+B.RELATIONAL OPERATOS -> Shows relations
+C.LOGICAL OPERATORS -> It gives logical output
+D.BITWISE OPERATORS -> OPERATIONS FORM OF 0'S AND 1'S
+E.ASSIGNMENT OPERATORS -> assignment of operator
+D.TARNARY OPERATORS -> Condtional operators
+
